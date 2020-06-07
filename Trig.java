e65fcb01e4b0542c0d4888c5f1fcca47307f045a
fmabud <66328308+fmabud@users.noreply.github.com>
2020-06-06 23:37:07 -0400

    Includes steps for half/double angle formulas

--- Trig.java
+++ Trig.java
@@ -46,28 +46,58 @@ public class Trig {
 		return Math.atan(radians);
 	}
 
-	public double halfsin() {
-		return Math.pow( (1 - Math.cos(radians)) / 2, 0.5 );
+	// Includes steps
+	public String halfsin() {
+		String output = "sin(theta / 2) = sqrt((1 - cos(theta)) / 2)" + "\n";
+	    output += "sin(" + (degrees / 2) + " degrees)) = sqrt((1 - cos(" + degrees + " degrees) / 2)" + "\n";
+	    output += "sin(" + (degrees / 2) + " degrees) = sqrt((1 - " + Math.cos(radians) + ") / 2)" + "\n";
+		output += "sin(" + (degrees / 2) + " degrees) = " + Math.pow( (1 - Math.cos(radians)) / 2, 0.5 );
+		return output;
 	}
 
-	public double halfcos() {
-		return Math.pow( (1 + Math.cos(radians)) / 2, 0.5 );
+	// Includes steps
+	public String halfcos() {
+	    String output = "cos(theta / 2) = sqrt((1 + cos(theta)) / 2)" + "\n";
+	    output += "cos(" + (degrees / 2) + " degrees)) = sqrt((1 + cos(" + degrees + " degrees) / 2)" + "\n";
+	    output += "cos(" + (degrees / 2) + " degrees) = sqrt((1 + " + Math.cos(radians) + ") / 2)" + "\n";
+		output += "cos(" + (degrees / 2) + " degrees) = " + Math.pow( (1 + Math.cos(radians)) / 2, 0.5 );
+		return output;
 	}
 
-	public double halftan() {
-		return Math.pow( (1 - Math.cos(radians)) / (1 + Math.cos(radians)) , 0.5 );
+	// Includes steps
+	public String halftan() {
+	    String output = "tan(theta / 2) = sqrt((1 - cos(theta)) / (1 + cos(theta)))" + "\n";
+	    output += "tan(" + (degrees / 2) + " degrees) = sqrt((1 - cos(" + degrees + " degrees)) / (1 + cos(" + degrees + " degrees)))" + "\n";
+	    output += "tan(" + (degrees / 2) + " degrees) = sqrt((1 - " + Math.cos(radians) + ") / (1 + " + Math.cos(radians) + "))" + "\n";
+		output += "tan(" + (degrees / 2) + " degrees) = " + Math.pow( (1 - Math.cos(radians)) / (1 + Math.cos(radians)) , 0.5 ) ;
+		return output;
 	}
 
-	public double doublesin() {
-		return ( 2 * Math.sin(radians) * Math.cos(radians) );
+	// Includes steps
+	public String doublesin() {
+	    String output = "sin(2 * theta) = 2 * sin(theta) * cos(theta)" + "\n";
+	    output += "sin (" + (2 * degrees) + " degrees) = 2 * sin(" + degrees + " degrees) * cos(" + degrees + " degrees)" + "\n";
+	    output += "sin (" + (2 * degrees) + " degrees) = 2 * " + Math.sin(radians) + " * " + Math.cos(radians) + "\n";
+		output += "sin (" + (2 * degrees) + " degrees) = " + ( 2 * Math.sin(radians) * Math.cos(radians) );
+		return output;
 	}
 
-	public double doublecos() {
-		return ( Math.pow(Math.cos(radians), 2) - Math.pow(Math.cos(radians), 2) );
+	// Includes steps
+	public String doublecos() {
+	    String output = "cos(2 * theta) = cos^2(theta) - sin^2(theta)" + "\n";
+	    output += "cos(" + (2 * degrees) + " degrees) = cos^2(" + degrees + " degrees) - sin^2(" + degrees + " degrees)" + "\n";
+	    output += "cos(" + (2 * degrees) + " degrees) = " + Math.pow(Math.cos(radians), 2) + " - " + Math.pow(Math.cos(radians), 2) + "\n";
+		output += "cos(" + (2 * degrees) + " degrees) = " + ( Math.pow(Math.cos(radians), 2) - Math.pow(Math.cos(radians), 2) );
+		return output;
 	}
 
-	public double doubletan() {
-		return ( 2 * Math.tan(radians) / (1 - Math.pow(Math.tan(radians), 2)) );
+	// Includes steps
+	public String doubletan() {
+		String output = "tan(2 * theta) = 2tan(theta) / (1 - tan^2(theta))" + "\n";
+		output += "tan(" + (2 * degrees) + " degrees) = 2tan(" + degrees + " degrees) / (1 - tan^2(" + degrees + " degrees))" + "\n";
+		output += "tan(" + (2 * degrees) + " degrees) = " + (2 * Math.tan(radians)) + " / (1 - " + Math.pow(Math.tan(radians), 2) + ")" + "\n";
+		output += "tan(" + (2 * degrees) + " degrees) = " + ( 2 * Math.tan(radians) / (1 - Math.pow(Math.tan(radians), 2)) );
+		return output;
 	}
 	
 	public String toString() {
@@ -80,12 +110,18 @@ public class Trig {
 		output += "asin(" + degrees + " degrees) =" + asin() + "\n";
 		output += "acos(" + degrees + " degrees) =" + acos() + "\n";
 		output += "atan(" + degrees + " degrees) =" + atan() + "\n";
-		output += "sin(" + (degrees / 2) + " degrees) =" + halfsin() + "\n";
-		output += "cos(" + (degrees / 2) + " degrees) =" + halfcos() + "\n";
-		output += "tan(" + (degrees / 2) + " degrees) =" + halftan() + "\n";
-		output += "sin(" + (degrees * 2) + " degrees) =" + doublesin() + "\n";
-		output += "cos(" + (degrees * 2) + " degrees) =" + doublecos() + "\n";
-		output += "tan(" + (degrees * 2) +" degrees) =" + doubletan();
+		output += "\n";
+		output += halfsin() + "\n";
+		output += "\n";
+		output += halfcos() + "\n";
+		output += "\n";
+		output += halftan() + "\n";
+		output += "\n";
+		output += doublesin() + "\n";
+		output += "\n";
+		output += doublecos() + "\n";
+		output += "\n";
+		output += doubletan();
 		return output;
 	}
 	
