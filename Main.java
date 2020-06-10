6312c80ab3901fd32044c7c74a6065ac7e4f34af
fmabud <66328308+fmabud@users.noreply.github.com>
2020-06-10 17:31:37 -0400

    Added back button

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 import java.awt.Graphics;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class Main extends JFrame {
+public class Main extends JFrame implements ActionListener {
   public Main() {
   	setDefaultCloseOperation(this.EXIT_ON_CLOSE);
   	setResizable(false);
+
+    JButton back = new JButton("Back");
+    back.addActionListener(this);
+
     Graphing x = new Graphing();
+    x.add(back);
+
     add(x);
     pack();
     setVisible(true);
-  }
+    }
 
+    public void actionPerformed(ActionEvent ae) {
+    	this.dispose();
+    	new Calculator();
+    }
 }
